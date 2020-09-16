8cdfe1a0dac9121c4d8a49eb711f29a8873e3023
vmwhoami <vmwhoami@gmail.com>
2020-09-16 18:27:17 +0300

    Add some sign in logic

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,12 @@
-class RegistrationsController < Device::RegistrationsController
-
+class RegistrationsController < Devise::RegistrationsController
+  before_action :sign_up_params, :account_update_params, if: :devise_controller?
   private
   def sign_up_params
-    parms.require(:user).permit(:name,:username,:email,:password,:password_confirmation)
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
   end
 
   def account_update_params
-    parms.require(:user).permit(:name,:username,:email,:password,:password_confirmation,:current_password)
+    params.require(:user).permit(:name,:username,:email,:password,:password_confirmation,:current_password)
   end
 
 
